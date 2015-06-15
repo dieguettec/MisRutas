01b0a692bcf652886b3a757233247b1e76446828
Diego Contreras <dcontrerasj@iberdrola.es>
2015-06-15 14:12:31 +0200

    funcionalidad basica

--- MisRutas/WebContent/js/app.js
+++ MisRutas/WebContent/js/app.js
@@ -1,3 +1,20 @@
+// crear bd
+var rutas = new Rutas();
+
+$(document).on('pageinit', '#pgHome', function() {
+	var vistaNuevaRuta = new NuevaRuta({collection: rutas, el: '#pgHome'});
+	});
+
+$(document).on('pageinit', '#pgMisRutas', function() {
+	var vistaEditarRuta = new EditarRuta({collection: rutas, el: '#pgEditarRuta'});
+	var vistaListaRutas = new ListaRutas({collection: rutas, el: '#pnRutas'});
+	vistaListaRutas.editarRuta(vistaEditarRuta);
+	});
+
+$(document).on('pageinit', '#pgMapa', function() {
+	var vistaMapa = new Mapa({collection: rutas, el: '#pgMapa'});
+	});
+/*
 $(document).one(
 		'pageshow',
 		'#pgMapa',
@@ -22,4 +39,4 @@ $(document).one(
 			};
 			var map = new google.maps.Map($('#pnMapa')[0], myOptions);
 		});
-
+*/
