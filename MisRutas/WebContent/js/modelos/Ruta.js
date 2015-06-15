31073b16073dc26cec835a5a07de5912b51563c8
Diego Contreras <dcontrerasj@iberdrola.es>
2015-06-15 09:14:30 +0200

    Primer Commit

---
+++ MisRutas/WebContent/js/modelos/Ruta.js
@@ -0,0 +1,18 @@
+/**
+* Clase que implementa rutas individuales
+*/
+var Ruta = Backbone.Model.extend({
+	initialize : function() {
+		if (!this.id)
+			this.set('id', _.uniqueId());
+		if (!this.has("posiciones"))
+			this.set('posiciones', []);
+		if (!this.has("fecha"))
+			this.set('fecha', Date());
+	},
+	defaults : {
+		titulo : 'Undefined',
+		visible : 'on',
+		color : '#000000',
+	},
+});
---
+++ MisRutas/WebContent/js/modelos/Rutas.js
@@ -0,0 +1,17 @@
+/**
+* Define una colección de rutas
+*/
+var Rutas = Backbone.Collection.extend({
+	model : Ruta,
+	initialize : function() {
+		this.on("add", function(model, col, opt) {
+			console.log('Rutas:add ' + model.id);
+		});
+		this.on("remove", function(model, col, opt) {
+			console.log('Rutas:remove ' + model.id);
+		});
+		this.on("change", function(model, opt) {
+			console.log('Rutas:change ' + model.id);
+		});
+	}
+});
